246c7ab5c6c220c5e103e8ebf04a1feabea5dfcd
Nicholas Porter <njporter10@gmail.com>
2016-08-02 11:06:49 -0700

    Got the twitter ajax call to be successful and append to the dom

--- SCscript.js
+++ SCscript.js
@@ -6,6 +6,7 @@
  */
 $(document).ready(function() {
     getSoundCloudSong();
+    getTwitterInfo();
 
 });
 // initialization
@@ -19,11 +20,37 @@ function getSoundCloudSong(){
         var player = $("#player");
         var artist = $('.artistName').val();
         artist = artist.replace(/\s/g, "_");
-
         SC.oEmbed('https://soundcloud.com/' + artist, {
             maxheight: 200
         }, function(res) {
             $("#player").html(res.html);
         });
+    });
+}
+
+function getTwitterInfo(){
+    $("#embedTrack").click(function() {
+        var feed = $("#twitter-feed");
+        var artist = $('.artistName').val();
+        $.ajax({
+            url: "http://s-apis.learningfuze.com/hackathon/twitter/index.php",
+            dataType: 'json',
+            method: 'POST',
+            data:{
+                search_term: artist
+            },
+            success: function (response) {
+                console.log('success!', response);
+                var list_of_tweets = response.tweets.statuses;
+                for(var i = 0; i <list_of_tweets.length; i++){
+                    $("#twitter-feed").append(list_of_tweets[i].text);
+                }
+
+            },
+            error: function (response) {
+                console.log('error!');
+            }
+
+        })
     });
 }
