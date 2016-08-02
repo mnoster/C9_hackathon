3b665cae4b7ad6f9dd777db30a4f0c98e15b847f
Nicholas Porter <njporter10@gmail.com>
2016-08-02 15:27:46 -0700

    got the formatting of the twitter box looking nice

--- SCscript.js
+++ SCscript.js
@@ -5,26 +5,27 @@
  * Created by njporter10 on 7/19/16.
  */
 $(document).ready(function() {
-    getSoundCloudSong();
+    getSoundCloudSong(); //when the document loads the api functions will be ready
     getTwitterInfo();
 
 });
 // initialization
-function getSoundCloudSong(){
-    SC.initialize({
+function getSoundCloudSong(){ //this is the function that holds the SOundcloud song player api
+    SC.initialize({ // this will initialize the client id needed for access to the SC api
         client_id: "15c5a12b5d640af73b16bd240753ffbb"
     });
 
     // Play audio
-    $("#embedTrack").click(function() {
-        var player = $("#player");
-        var artist = $('.artistName').val();
+    $("#embedTrack").click(function() { //on the click of the submit button in the html
+        var player = $("#SCplayer"); //the variable of SC player will be set to the id of the song media player
+        var artist = $('.artistName').val();  // the artist name will be deifined as a string of text in the
         artist = artist.replace(/\s/g, "_");
+        artist = artist.replace(".", "_");
         SC.oEmbed('https://soundcloud.com/' + artist, {
             maxheight: 150,
             maxwidth: 800
         }, function(res) {
-            $("#player").html(res.html);
+            $("#SCplayer").html(res.html);
         });
     });
 }
@@ -46,14 +47,16 @@ function getTwitterInfo(){
                 var tweet_array = [];
                 for(var i = 0; i <list_of_tweets.length; i++){
                     tweet_array.push(list_of_tweets[i].text);
-                    $('<tr>').addClass('col-lg-4 col-md-5 col-xs-6').text(tweet_array[i]).appendTo('tbody');
-                    // $("#twitter-feed").append(tweet_array[i]);
+                    $('<tr>').addClass('twitter_border_lines').text(tweet_array[i]).appendTo('tbody');
                 }
-                console.log('tweet array :' , tweet_array);
+                $('.contain-tweets').css("visibility","visible");
+
 
             },
             error: function (response) {
                 console.log('error!');
+                $('.contain-tweets').css("visibility","visible");
+                $('tbody').text("Oh no! There was an error with Twitter's server.").css({'color': 'red'});
             }
 
         })
